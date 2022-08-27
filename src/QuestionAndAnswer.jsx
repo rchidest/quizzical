dec5cf1986759c5766bf081482506594c2b35f73
Richard <richard.chidester@gmail.com>
2022-08-27 00:56:03 -0600

    check answers implemented

--- src/QuestionAndAnswer.jsx
+++ src/QuestionAndAnswer.jsx
@@ -1,38 +1,55 @@
-import { convert } from "html-to-text";
+import { convert } from 'html-to-text'
 
 export default function QuestionAndAnswer(props) {
-  const { quizData } = props;
+  const { quizData, checkAnswers } = props
 
   function Answers(props) {
+    const { answers } = props
     return (
-      <div className="answers">
-        {props.answers.map((answer) => (
+      <div className='answers'>
+        {answers.map((answer) => (
           <Answer key={answer.id} answer={answer} />
         ))}
       </div>
-    );
+    )
   }
   function Answer(props) {
-    const answer = props.answer;
+    const { answer } = props
+
+    const selectedAnswerColor = '#D6DBF5'
+    const unselectedAnswerColor = 'white'
+    const correctAnswerColor = '#94D7A2'
+    const wrongtAnswerColor = '#F8BCBC'
+
+    let bgColor = unselectedAnswerColor
+    if (checkAnswers) {
+      if (answer.isAnswer) {
+        bgColor = correctAnswerColor
+      } else if (answer.isSelected) {
+        bgColor = wrongtAnswerColor
+      }
+    } else {
+      bgColor = answer.isSelected ? selectedAnswerColor : unselectedAnswerColor
+    }
     const styles = {
-      backgroundColor: answer.isSelected ? "#D6DBF5" : "white",
-    };
+      backgroundColor: bgColor,
+    }
 
     return (
       <button
-        className="answer"
+        className='answer'
         style={styles}
         onClick={() => answer.selectAnswer(answer.id)}
       >
         {convert(answer.answer)}
       </button>
-    );
+    )
   }
   return (
     <div>
-      <div className="question">{convert(quizData.question)}</div>
+      <div className='question'>{convert(quizData.question)}</div>
       <Answers answers={quizData.answers} />
-      <div className="line"></div>
+      <div className='line'></div>
     </div>
-  );
+  )
 }
--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,127 +1,172 @@
-import React from "react";
-import QuestionAndAnswer from "./QuestionAndAnswer";
-import { nanoid } from "nanoid";
+import React from 'react'
+import QuestionAndAnswer from './QuestionAndAnswer'
+import { nanoid } from 'nanoid'
 
 export default function Questions(props) {
-  const [questionsAndAnswers, setQuestionsAndAnswers] = React.useState([]);
-  const [checkAnswers, setCheckAnswers] = React.useState(false);
-  const [showCorrectAnswers, setShowCorrectAnswers] = React.useState(0);
-  const [replay, setReplay] = React.useState(false);
-  const [selectedAnswer, setSelectedAnswer] = React.useState("");
+  const [questionsAndAnswers, setQuestionsAndAnswers] = React.useState([])
+  const [checkAnswers, setCheckAnswers] = React.useState(false)
+  const [correctAnswers, setCorrectAnswers] = React.useState(0)
+  const [replay, setReplay] = React.useState(false)
+  const [selectedAnswer, setSelectedAnswer] = React.useState('')
 
   React.useEffect(() => {
     if (!checkAnswers) {
-      fetch("https://opentdb.com/api.php?amount=5").then((resp) =>
+      fetch('https://opentdb.com/api.php?amount=5').then((resp) =>
         resp.json().then((data) => {
           setQuestionsAndAnswers(
             data.results.map((qAndA) => generateQAndA(qAndA))
-          );
+          )
         })
-      );
+      )
     }
-  }, [replay, checkAnswers]);
+  }, [replay, checkAnswers])
 
   React.useEffect(() => {
     if (selectedAnswer) {
       const newQAndAs = questionsAndAnswers.map((qAndA) => {
         const newAnswers = qAndA.answers.map((answer) => {
-          const newAnswer =
-            selectedAnswer === answer.id
-              ? { ...answer, isSelected: !answer.isSelected }
-              : { ...answer, isSelected: false };
-          return newAnswer;
-        });
-        qAndA.answers = newAnswers;
-        return { ...qAndA };
-      });
-      setQuestionsAndAnswers(newQAndAs);
+          let newAnswer = {}
+          const { id, qId, isSelected } = answer
+          if (selectedAnswer === id) {
+            console.log(
+              `qId=${qId}  isSelected=${isSelected}  qAndA.id=${qAndA.id}`
+            )
+            newAnswer = { ...answer, isSelected: !isSelected }
+          } else {
+            newAnswer = { ...answer }
+          }
+
+          return newAnswer
+        })
+        qAndA.answers = newAnswers
+        return { ...qAndA }
+      })
+      setQuestionsAndAnswers(newQAndAs)
     }
-  }, [selectedAnswer]);
+  }, [selectedAnswer])
+
+  React.useEffect(() => {
+    if (checkAnswers) {
+      let numCorrectAnswers = 0
+      questionsAndAnswers.forEach((qAndA) => {
+        qAndA.answers.forEach((answer) => {
+          if (answer.isSelected && answer.isAnswer) {
+            numCorrectAnswers++
+          }
+        })
+      })
+      setCorrectAnswers(numCorrectAnswers)
+    }
+  }, [checkAnswers])
 
   function generateQAndA(qAndA) {
+    const id = nanoid()
     return {
-      id: nanoid(),
+      id: id,
       question: qAndA.question,
-      answers: generateAnswers(qAndA.correct_answer, qAndA.incorrect_answers),
-    };
+      answers: generateAnswers(
+        id,
+        qAndA.correct_answer,
+        qAndA.incorrect_answers
+      ),
+    }
   }
-  function generateAnswers(correct_answer, incorrect_answers) {
-    const answers = [];
-    const id = nanoid();
+  function generateAnswers(qId, correct_answer, incorrect_answers) {
+    const answers = []
+    const id = nanoid()
     answers.push({
+      qId: qId,
       id: id,
       answer: correct_answer,
       isAnswer: true,
       isSelected: false,
       selectAnswer: selectAnswer,
-    });
+    })
     incorrect_answers.map((answer) => {
-      const id = nanoid();
+      const id = nanoid()
       answers.push({
+        qId: qId,
         id: id,
         answer: answer,
         isAnswer: false,
         isSelected: false,
         selectAnswer: selectAnswer,
-      });
-    });
-    return shuffle(answers);
+      })
+    })
+    return shuffle(answers)
   }
   function shuffle(array) {
     let currentIndex = array.length,
-      randomIndex;
+      randomIndex
 
     // While there remain elements to shuffle.
     while (currentIndex != 0) {
       // Pick a remaining element.
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
+      randomIndex = Math.floor(Math.random() * currentIndex)
+      currentIndex--
 
       // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
+      ;[array[currentIndex], array[randomIndex]] = [
         array[randomIndex],
         array[currentIndex],
-      ];
+      ]
     }
 
-    return array;
+    return array
   }
 
   function selectAnswer(id) {
-    setSelectedAnswer(id);
+    setSelectedAnswer(id)
+  }
+  function findAnswer(id) {
+    let theAnswer = {}
+    questionsAndAnswers.forEach((qAndA) => {
+      qAndA.answers.forEach((answer) => {
+        console.log(`answer.id=${answer.id}  id=${id}`)
+        if (answer.id === id) {
+          theAnswer = answer
+        }
+      })
+    })
+    console.log(`theAnswer.id=${theAnswer.id}`)
+    return theAnswer
   }
 
   function getTheAnswers() {
-    setCheckAnswers(true);
+    setCheckAnswers(true)
   }
 
   function playAgain() {
-    setCheckAnswers(false);
-    setReplay(true);
+    setCheckAnswers(false)
+    setReplay(true)
   }
 
   return (
-    <div className="quiz">
+    <div className='quiz'>
       <div>
         {questionsAndAnswers.length > 0 &&
           questionsAndAnswers.map((qAndA) => (
-            <QuestionAndAnswer key={qAndA.id} quizData={qAndA} />
+            <QuestionAndAnswer
+              key={qAndA.id}
+              quizData={qAndA}
+              checkAnswers={checkAnswers}
+            />
           ))}
       </div>
       {!checkAnswers ? (
-        <button className="quiz-btn" onClick={getTheAnswers}>
+        <button className='quiz-btn' onClick={getTheAnswers}>
           Check answers
         </button>
       ) : (
-        <div className="play-again">
-          <h5 className="answer-score">
-            You scored {showCorrectAnswers}/5 correct answers
+        <div className='play-again'>
+          <h5 className='answer-score'>
+            You scored {correctAnswers}/5 correct answers
           </h5>
-          <button className="quiz-btn" onClick={playAgain}>
+          <button className='quiz-btn' onClick={playAgain}>
             Play again
           </button>
         </div>
       )}
     </div>
-  );
+  )
 }
